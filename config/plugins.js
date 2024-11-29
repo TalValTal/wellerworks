7482e5075adb9fe2b97089dd51aa2bbe947ca269
Tal Valler <talvaller@hotmail.com>
2024-11-29 22:29:23 +0000

    Update plugins configuration

--- config/plugins.js
+++ config/plugins.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   'users-permissions': {
     config: {
-      jwtSecret: env('JWT_SECRET')
+      jwtSecret: env('JWT_SECRET', 'b1a9c6e2f5d8447890123456789abcdef')
     },
   },
 });
