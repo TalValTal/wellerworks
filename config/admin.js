f7fb4d121a95929ca5c374c4fc50ea80cd14c79f
Tal Valler <talvaller@hotmail.com>
2024-11-29 23:18:55 +0000

    Update admin config

--- config/admin.js
+++ config/admin.js
@@ -1,17 +1,19 @@
 module.exports = ({ env }) => ({
-  auth: {
-    secret: 'f5d8b1a9c6e2447890123456789abcdef',
-  },
-  apiToken: {
-    salt: 'MoX8Thqvsdx2Z8db76IctQ=='  // Replace with your actual API token salt
-  },
-  transfer: {
-    token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+  admin: {
+    auth: {
+      secret: 'f5d8b1a9c6e2447890123456789abcdef',
     },
-  },
-  flags: {
-    nps: env.bool('FLAG_NPS', true),
-    promoteEE: env.bool('FLAG_PROMOTE_EE', true),
+    apiToken: {
+      salt: 'MoX8Thqvsdx2Z8db76IctQ=='
+    },
+    transfer: {
+      token: {
+        salt: env('TRANSFER_TOKEN_SALT'),
+      },
+    },
+    flags: {
+      nps: env.bool('FLAG_NPS', true),
+      promoteEE: env.bool('FLAG_PROMOTE_EE', true),
+    },
   },
 });
